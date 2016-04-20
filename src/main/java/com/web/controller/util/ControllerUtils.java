ac55822e4eaa72f3dc53a8a5ce3c54c22d1bf004
saikeerthi57 <keerthijkb@gmail.com>
2016-04-20 16:45:36 -0400

    using angular js

--- src/main/java/com/web/controller/util/ControllerUtils.java
+++ src/main/java/com/web/controller/util/ControllerUtils.java
@@ -3,16 +3,19 @@ package com.web.controller.util;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 
-public final class ControllerUtils {
 
-    public static final String JSON_UTF8 = MediaType.APPLICATION_JSON_VALUE  + ";charset=UTF-8";
-    public static final String XML_UTF8 = MediaType.TEXT_XML_VALUE  + ";charset=UTF-8";
+    public final class ControllerUtils {
 
-    private ControllerUtils() {
+        public static final String JSON_UTF8 = MediaType.APPLICATION_JSON_VALUE  + ";charset=UTF-8";
+        public static final String XML_UTF8 = MediaType.TEXT_XML_VALUE  + ";charset=UTF-8";
 
+        private ControllerUtils() {
+
+        }
+
+        public static boolean isPagingDisabled(Pageable pageable) {
+            return pageable == null || (pageable.getPageSize() == Integer.MAX_VALUE && pageable.getSort() == null);
+        }
     }
 
-    public static boolean isPagingDisabled(Pageable pageable) {
-        return pageable == null || (pageable.getPageSize() == Integer.MAX_VALUE && pageable.getSort() == null);
-    }
-}
+
