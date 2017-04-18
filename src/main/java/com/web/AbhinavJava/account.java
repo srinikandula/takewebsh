6782b4b344adac64a41a02d9aa2ce3aeb4911674
surapaneniabhinav <surapaneni.abhinav@gmail.com>
2017-04-18 19:18:41 -0400

    Bank Application

--- src/main/java/com/web/AbhinavJava/account.java
+++ src/main/java/com/web/AbhinavJava/account.java
@@ -1,24 +1,35 @@
-class account{
-        private int id;
-        private String name;
-        private double balance;
-        
-        public void setId(int num){
-            id = num;
-        }
-        public void setName(String nam){
-            name = nam;
-        }
-        public void setBalance(double bal){
-            balance = bal;
-        }
-        public int getId(){
-            return id;
-        }
-        public String getName(){
-            return name;    
-        }
-        public double getBalance(){
-            return balance;
-        }
+package com.web.AbhinavJava;
+
+import java.io.Serializable;
+
+/**
+ * Created by surap on 4/18/2017.
+ */
+
+public class Account implements Serializable {
+    private int accountId;
+    private String name;
+    private double balance;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getBalance() {
+        return balance;
+    }
 }
