903c41a61ac02a99663da682275a0a7d2167c816
surapaneniabhinav <surapaneni.abhinav@gmail.com>
2017-07-07 13:29:03 -0400

    practise change

--- src/main/java/com/web/AbhinavJava/account.java
+++ src/main/java/com/web/AbhinavJava/account.java
@@ -1,27 +1,22 @@
 package com.web.AbhinavJava;
 
-import java.io.Serializable;
-
 /**
  * Created by surap on 4/18/2017.
  */
 
-public class Account implements Serializable {
+class Account {
     private int accountId;
     private String name;
     private double balance;
+    private transient long ssn;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
+    public void setBalance(Double balance) { this.balance = balance; }
 
-    public void setAccountId(int accountId) {
-        this.accountId = accountId;
-    }
+    public void setAccountId(int accountId){ this.accountId = accountId; }
+
+    public void setSsn(long ssn) { this.ssn = ssn; }
 
     public int getAccountId() {
         return accountId;
@@ -32,4 +27,5 @@ public class Account implements Serializable {
     public double getBalance() {
         return balance;
     }
+    public long getSsn(){return ssn;}
 }
