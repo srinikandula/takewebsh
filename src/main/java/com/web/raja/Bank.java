33ec24fac0239c3280f6c0b1d1c2cf8db018457b
sriharshakota <sharsha142@gmail.com>
2017-07-12 13:36:35 -0400

    JDBC bank example

--- src/main/java/com/web/raja/Bank.java
+++ src/main/java/com/web/raja/Bank.java
@@ -1,11 +1,1 @@
-/**
- * Created by HOME on 4/7/2017.
- */
-public class Bank {
-    public static void main(String[] args) {
-        Account account= new Account();
-           account.setBalance(2000);
-        System.out.println(account.getBalance());
-        }
-    }
 
